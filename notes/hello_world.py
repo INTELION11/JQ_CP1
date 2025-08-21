8daa3f19144089c3b99d16bea2f0d359f65c6274
INTELION11 <jacob.quezada@ucas-edu.net>
2025-08-21 09:52:35 -0600

    variable notes

--- notes/hello_world.py
+++ notes/hello_world.py
@@ -1,3 +1,12 @@
+
+import time
+num = 0
+for i in range (100):
+    print(num)
+    num = num +1
+    time.sleep(0.01)
+
+
 name= input("what is your name?").capitalize ()
 print("hello", name)
 print("welcome adventurer", name, " i need you to finish a quest for me")
@@ -11,3 +20,4 @@ else:
    # addition is done my putting print  and then the problen i parenthesis, no quotation marks
     print(2+2)
     #crtl alt and down or up arow to select multiple lines
+   
---
+++ notes/variables.py
@@ -0,0 +1,8 @@
+# JQ 1st Variable notes
+
+even_number = 2
+grade = 99
+animal = "Panda"
+print(grade)
+grade = 95
+print(grade)
