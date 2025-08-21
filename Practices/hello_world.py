0ba6c567c3eda6b7369d10507dc013b2673b1839
INTELION11 <jacob.quezada@ucas-edu.net>
2025-08-21 00:03:09 +0000

    yo

--- Practices/hello_world.py
+++ Practices/hello_world.py
@@ -1,7 +1,3 @@
 # JQ  1st  Hello World python
-returning_users= ["Lobotomy","Jack","Mary","Alex","Steve"]
-name= input("what is your name?").capitalize ()
-if name in returning_users:
-    print("welcome back",name)
-else:
-    print("hello",name)
+name= input ("what is your name:").capitalize ()
+print("hello",name)
