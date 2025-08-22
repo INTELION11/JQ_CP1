d04d3760a23ce711bfe924805ad3149050c4af99
INTELION11 <jacob.quezada@ucas-edu.net>
2025-08-22 09:38:57 -0600

    lard

---
+++ Practices/who_are_you.py
@@ -0,0 +1,8 @@
+# JQ 1st Who are you?
+
+
+while True:
+    name = input (" What is your name? \n")
+    age =  input ("what is your age? \n")
+    fav_color = input ("what is your favorite color? \n")
+    print ("Hello",name,"your",age,"years old and love the color",fav_color,)
