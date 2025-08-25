f08aacb0732aef5394229f75b289b5b6489aa96e
INTELION11 <jacob.quezada@ucas-edu.net>
2025-08-25 09:09:21 -0600

    man

--- projects/who_are_you.py
+++ projects/who_are_you.py
@@ -1,8 +1,6 @@
 # JQ 1st Who are you?
-
-
 while True:
     name = input (" What is your name? \n")
     age =  input ("what is your age? \n")
     fav_color = input ("what is your favorite color? \n")
-    print ("Welcome",name,"your",age,"years old and love the color",fav_color,"!!")
+    print ("Welcome",name,"you're",age,"years old and love the color",fav_color,"!!")
