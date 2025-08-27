28747b509e7af44510abfe99c339a98c4994049c
INTELION11 <jacob.quezada@ucas-edu.net>
2025-08-27 09:52:43 -0600

    lamb

---
+++ notes/numbers.py
@@ -0,0 +1,42 @@
+# JQ 1st Intigers and floats notes
+
+# Itigers
+#dnt require special info to make happen
+age = 9999999999999999
+year= 2015
+room = 200
+hour = 9
+motivation = -5
+
+#float (Floating point number)
+
+pi = 3.14
+percent = -.75
+damage = 9.75
+
+#math
+print (5.0+2) #7
+print (5-2) #3
+print (5*2) #10
+print (5/2) # 2.5
+print (5//2) #2
+print (5%2) #1
+print (5**2) #25
+
+print (5+2 *9 + (5**5)%3)
+
+#type casting
+num = int(3.14) # does not round!
+decimal = float(8)
+print (num)
+print(decimal)
+
+#Using variables
+hours = 3
+minutes = 4
+
+print(hours*60+minutes)
+
+age = int(input("how old are you?\n"))  # surround it by an intiger for it to work
+
+print("wow you are old, that is ", 525600*age, "minutes!")
