b40323d6ce3fcc275bfc46560f03e6f7c80d09c1
INTELION11 <jacob.quezada@ucas-edu.net>
2025-08-28 09:53:00 -0600

    proj

---
+++ projects/average_grade.py
@@ -0,0 +1,13 @@
+# JQ 1st Avarage Numbers Practice
+while True:
+    class_grd1 = float(input("What is your first period grade?"))
+    class_grd2 = float(input("What is your second period grade?"))
+    class_grd3 = float(input("What is your third period grade?"))
+    class_grd4 = float(input("What is your fourth period grade?"))
+    class_grd5 = float(input("What is your fifth period grade?"))
+    class_grd6 = float(input("What is your sixth period grade?"))
+    class_grd7 = float(input("What is your seventh period grade?"))
+    total = class_grd1 + class_grd2 + class_grd3 + class_grd4 + class_grd5 + class_grd6 + class_grd7 
+    avarge = (total / 7)
+    avarge_rounded = round(avarge,2)
+    print(avarge_rounded)
