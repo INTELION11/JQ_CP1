627095b543d1ca202caa3b7b8cf195f55bddfd5a
INTELION11 <jacob.quezada@ucas-edu.net>
2025-09-02 09:52:41 -0600

    mag

---
+++ notes/strings.py
@@ -0,0 +1,30 @@
+#JQ 1st String Notes
+
+#what is a string
+
+
+#string example
+sentence = "The quick brown fox jumps over the lazy dog!"
+
+first_name = "Tia"
+month = "september"
+book = "the return of the king"
+food = "chocolate"
+leinghth = len(sentence)
+print(sentence)
+print("this sentence is", leinghth, "characters long")
+
+
+print ('\t"Inkheart" is the best book ever')
+# escape variable
+print("\"Inkheart\" is the best book\n ever")
+
+#concatnate
+last_name = "LaRose"
+full_name = first_name + " " + last_name
+
+print(full_name)    
+
+#index
+print(last_name[0:3])
+print(sentence[10:15])
