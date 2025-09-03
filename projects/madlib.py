63288dede94bd5bde7dceda968f37cd7ab275e9b
INTELION11 <jacob.quezada@ucas-edu.net>
2025-09-03 09:51:56 -0600

    lem

---
+++ projects/madlib.py
@@ -0,0 +1,13 @@
+# JQ 1st Madlib
+adject1 = input("give me an adjective ")
+adject2 = input("give me another adjective ")
+lizartype = input("give me a type of reptile ")
+Room = input(" give me a room in a house ")
+pastverb1 = input("give me a verb in past tense ")
+verb1 = input("give me a verb ")
+name = input("give me a name ")
+noun = input("give me a noun ")
+radioac = input("give me a radioactive item ")
+verbing1 = input("give me a verb ending in -ing ")
+story = "it was a " + adject1 + ", cold November day. I woke up to the " + adject2 + " smell of " + lizartype + " roasting in the " + Room + " downstairs. I " + pastverb1 + " down the stairs to see if i could help " + verb1 + ' the dinner, my mom said, "see if ' + name + " needs a fresh set of " + radioac + " , but right then i started " + verbing1 + " and could not stop."
+print(story)
