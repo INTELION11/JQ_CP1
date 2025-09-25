a7ef737591dc61751367518065cd7ad27d4c8e5d
INTELION11 <jacob.quezada@ucas-edu.net>
2025-09-25 09:53:32 -0600

    debugging

---
+++ notes/Debugging.py
@@ -0,0 +1,16 @@
+# JQ 1st DEBUGGING NOTES!
+
+
+#Syntax Error - grammar error
+print"Vienna"
+# => read error message, go to that line of code, fix the syntax
+
+#Logic Errors - we gave the wrong steps
+numone = "2"
+numtwo = "5"
+
+print (numone + numtwo)
+
+# Have well thought out plan, step by step go thorough our logic
+
+# Run-time Error
--- projects/letter_grade.py
+++ projects/letter_grade.py
@@ -1,30 +1,31 @@
 #JQ 1st Letter grade
-grade = float(input("What is your grade? "))
-if grade >= 92:
-    print(f"your {grade}% is equal to an A")
-elif  grade >= 90 and grade <= 91:
-    print(f"your {grade}% is equal to an -A")
-elif  grade >= 87 and grade <= 89:
-    print(f"your {grade}% is equal to an B+")
-elif  grade >= 82 and grade <= 86:
-    print(f"your {grade}% is equal to an B")
-elif  grade >= 80 and grade <= 81:
-    print(f"your {grade}% is equal to an B-")
-elif  grade >= 77 and grade <= 79:
-    print(f"your {grade}% is equal to an C+,you can stil tryout for janitor")
-elif  grade >= 72 and grade <= 76:
-    print(f"your {grade}% is equal to an C, I could climb up your ego skydive down to your IQ level  ")
-elif  grade >= 70 and grade <= 71:
-    print(f"your {grade}% is equal to an C-, if you where 1/2 as smart as you think you are you would be twice as smart as you really are")
-elif  grade >= 67 and grade <= 69:
-    print(f"your {grade}% is equal to an D+, if your brain was dynamite it would not be enough to blow your head off")
-elif  grade >= 64 and grade <= 66:
-    print(f"your {grade}% is equal to an D, the only test you got above avarage was the chromazone test")
-elif  grade >= 55 and grade <= 63:
-    print(f"your {grade}% is equal to an D-, you probably scored on the gene pool too")
-elif  grade >= 0 and grade <= 54:
-    print(f"your {grade}% is equal to an F, i cant even tell myself tell myslef im 10x smarter than you since 10 x 0 = 0")
-else: 
-    print("There is a problem ,you know nothing and thinks he knows everything. That points clearly to a political career. \n")
-    
-    
+while True:
+    grade = float(input("What is your grade? "))
+    if grade >= 92:
+        print(f"your {grade}% is equal to an A, could have done better")
+    elif  grade >= 90 and grade <= 91:
+        print(f"your {grade}% is equal to an -A, not an A+? you dont deserve anything")
+    elif  grade >= 87 and grade <= 89:
+        print(f"your {grade}% is equal to an B+, i see, you lazy good for nothing")
+    elif  grade >= 82 and grade <= 86:
+        print(f"your {grade}% is equal to an B, Below avarage, as expected but im stil disapointed")
+    elif  grade >= 80 and grade <= 81:
+        print(f"your {grade}% is equal to an B-, bother is on the verge of failing")
+    elif  grade >= 77 and grade <= 79:
+        print(f"your {grade}% is equal to an C+,you can stil tryout for janitor")
+    elif  grade >= 72 and grade <= 76:
+        print(f"your {grade}% is equal to an C, I could climb up your ego skydive down to your IQ level  ")
+    elif  grade >= 70 and grade <= 71:
+        print(f"your {grade}% is equal to an C-, if you where 1/2 as smart as you think you are you would be twice as smart as you really are")
+    elif  grade >= 67 and grade <= 69:
+        print(f"your {grade}% is equal to an D+, if your brain was dynamite it would not be enough to blow your head off")
+    elif  grade >= 64 and grade <= 66:
+        print(f"your {grade}% is equal to an D, the only test you got above avarage was the chromazone test")
+    elif  grade >= 55 and grade <= 63:
+        print(f"your {grade}% is equal to an D-, you probably scored on the gene pool too")
+    elif  grade >= 0 and grade <= 54:
+        print(f"your {grade}% is equal to an F, i cant even tell myself tell myslef im 10x smarter than you since 10 x 0 = 0")
+    else: 
+        print("There is a problem ,you know nothing and thinks he knows everything. That points clearly to a political career. \n")
+        
+        
