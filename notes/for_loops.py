5a795c4e51bcfb611caab93b084663f2800006b7
INTELION11 <jacob.quezada@ucas-edu.net>
2025-10-01 09:43:30 -0600

    for loops

---
+++ notes/for_loops.py
@@ -0,0 +1,30 @@
+# JQ 1st For Loops Notes
+import time
+nums = [4,654,136,84,651,43,63254,34,5,36]
+
+# FOR Num(Current item) IN Nums(list)
+for num in nums:
+    print(num)
+    num /= 2
+    if num > 100:
+        print(f"{num} is only half of {num*2}. it is  large number")
+    else:
+        print(num)
+print("the loop is over")
+
+for num in range(1,1000000000000,100000) :
+    time.sleep(.00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001)
+    print(num)
+    print(num)
+    print(num)
+    print("noob")
+    print("noo0-00000000000000b")
+    print("n     ")
+    print("      ")
+    print(num)
+    print(num)
+    print(num)
+    print("noob")
+    print("noo0-00000000000000b")
+    print("n     ")
+    print("      ")
