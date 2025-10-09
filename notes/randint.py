a40c15dc8e9ceeac1f2614ac3b397432789d6ec2
INTELION11 <jacob.quezada@ucas-edu.net>
2025-10-09 09:53:17 -0600

    phseudowoodo

---
+++ notes/randint.py
@@ -0,0 +1,24 @@
+if 1 is 0 and 2 is 0 and 3 is 0
+      display player has won
+      won is 0
+if 4 is 0 and 5 is 0 and 6 is 0 
+      display player has won
+      won is 0
+if 7 is 0 and 8 is 0 and 9 is 0 
+      display player has won
+      won is 0
+if 1 is 0 and 4 is 0 and 7 is 0 
+      display player has won
+      won is 0
+if 2 is 0 and 5 is 0 and 8 is 0 
+      display player has won
+      won is 0
+if 3 is 0 and 6 is 0 and 9 is 0 
+      display player has won
+      won is 0
+if 1 is 0 and 5 is 0 and 9 is 0 
+      display player has won
+      won is 0
+if 3 is 0 and 5 is 0 and 7 is 0 
+      display player has won
+      won is 0
