19d0f42ee276df2be7db173ce95142de76a4f8e1
INTELION11 <jacob.quezada@ucas-edu.net>
2025-10-10 09:53:26 -0600

    password

---
+++ Practices/random_num_gen.py
@@ -0,0 +1,24 @@
+# JQ 1st random num gen
+player = [1,2,3]
+
+if player == [1][2][3]:
+    print("you won") 
+else: 
+    print("")
+if player == [4][5][6]:
+    print("you won") 
+else:
+    print("")
+if player == [7][8][9]:
+    print("you won") 
+else:
+    print("") 
+
+if player == [1][5][9]:
+     print("you won") 
+else:
+    print("")
+if player == [7][5][3]: 
+    print("you won") 
+else:
+    print("")
--- notes/randint.py
+++ notes/randint.py
@@ -1,24 +1,12 @@
-if 1 is 0 and 2 is 0 and 3 is 0
-      display player has won
-      won is 0
-if 4 is 0 and 5 is 0 and 6 is 0 
-      display player has won
-      won is 0
-if 7 is 0 and 8 is 0 and 9 is 0 
-      display player has won
-      won is 0
-if 1 is 0 and 4 is 0 and 7 is 0 
-      display player has won
-      won is 0
-if 2 is 0 and 5 is 0 and 8 is 0 
-      display player has won
-      won is 0
-if 3 is 0 and 6 is 0 and 9 is 0 
-      display player has won
-      won is 0
-if 1 is 0 and 5 is 0 and 9 is 0 
-      display player has won
-      won is 0
-if 3 is 0 and 5 is 0 and 7 is 0 
-      display player has won
-      won is 0
+my_list = [1, 2, 3, "apple", 5, "banana"]
+element_to_find = "apple"
+
+# Check if the element is in the list
+found_true_or_false = element_to_find in my_list
+
+print(f"Is '{element_to_find}' in the list? {found_true_or_false}")
+
+element_to_find_not_present = "grape"
+found_true_or_false_not_present = element_to_find_not_present in my_list
+
+print(f"Is '{element_to_find_not_present}' in the list? {found_true_or_false_not_present}")
