af5aaed49d51d17954281e10e02f7e197d3d0e42
INTELION11 <jacob.quezada@ucas-edu.net>
2025-10-23 02:06:51 +0000

    password cheker

--- projects/password-Streinght.py
+++ projects/password-Streinght.py
@@ -1,120 +1,75 @@
-    #JQ 1st Paasword streinth
-    #password is ask "what is your password"
-    #points = 0
-while True:
-    password = input("what is your password \n").strip()
-    points = 0
-    num_points = 0
-    spec_points = 0
-    upperc_points = 0
-    lowerc_points = 0
-
-
-    #length = len(password)
-    length = len(password)
-    #list special_character
-    special_character = ("!@#$%^&*()_+-=[]{|;:,}.<>?)")
-    #list numbers
-    numbers = ("1234567890")
-    #characters is list(password)
-    characters = (password)
-    #loop for letter in characters
-
-
-    for i in characters:
-        numb = i.isdigit()
-        if numb:
-            points = points + 1
-            print("Your password contains a number character! +1 point")
-            break
-        else: continue
-
-
-
-    for i in characters:
-    #    special is bool([special_character].find(letter))
-        special = bool((i).find(special_character))
-    #    num is bool([numbers].find(letter))
-        num = bool((i).find(numbers))
-    #    upperc is bool(password isupper.)
-        upperc = i .isupper()
-    #    lowerc is bool(password islower.)
-        lowerc =  i .islower()
-    #    if special is true then spec_points += 1
-        if special == True:
-            spec_points += 1
-        else: 
-            print("")
-    #    if num is true then num_points += 1
-        if num == True:
-            num_points += 1
-        else: 
-            print("")
-    #    if upperc is true then upper_points += 1
-        if upperc == True:
-            upperc_points += 1
-        else: 
-            print("")
-    #    if lowerc is true then lower_points += 1
-        if lowerc == True:
-            lowerc_points += 1
-        else: 
-            print("")
-    #if special >= 1 then points += 1
-    if special >= 1:
-        points += 1
-    else:
-        Points = points
-    #if num >= 1 then points += 1
-    if num >= 1:
-        points += 1
-    else:
-        Points = points
-    #if upperc >= 1 then points += 1
-    if upperc >= 1:
-        points += 1
-    else:
-        Points = points
-    #if lowerc >= 1 then points += 1
-    if lowerc >= 1:
-        points += 1
-    else:
-        Points = points
-    #if length >= 8 then points += 1
-    if length >= 8:
-        points += 1
-    else:
-        Points = points
-
-
-    #Meeting the length requirement == True: +1 point
-    #Containing uppercase letters == True: +1 point
-    #Containing lowercase letters == True: +1 point
-    #Containing numbers == True: +1 point
-    #Containing special characters == True: +1 point
-
-    #if points == 1
-    if points == 1:
-        print("password is weak \n")
-        print("1 point")
-    #display password is weak
-    #if points == 2
-    elif points == 2:
-        print("password is weak \n")
-        print("2 points")
-    #display password is weak
-    #if points == 3
-    elif points == 3:
-        print("password is medium\n")
-    #display password is medium
-    #if points == 4
-    elif points == 4:
-        print("password is good \n")
-    #display password is good
-    #if points == 5
-    elif points == 5:
-        print("password is strong \n")
-    else: 
-        print("please try again")
-    #display password is strong
-    #else display please try again
+#JQ 1st Paasword streinth  
+#password is ask "what is your password"  
+#points = 0  
+while True:  
+    password = input("what is your password \n").strip()  # ask for password and clean it  
+    points = 0  # start points at zero  
+  
+    #length = len(password)  
+    #list special_character  
+    #list numbers  
+    #characters is list(password)  
+    #loop for letter in characters  
+  
+    special_characters = "!@#\$%^&*()_+-=[]{|;:,}.<>?)"  # all the weird symbols  
+    numbers = "1234567890"  # just the digits for checking  
+    # set up flags to see if each thing is found  
+    has_num = False  # did we see a number yet  
+    has_special = False  # did we see a special character yet  
+    has_upper = False  # any uppercase found  
+    has_lower = False  # any lowercase found  
+  
+    # go through each character in password  
+    for char in password:  
+        # if it's a number, remember that  
+        if char in numbers:  
+            has_num = True  
+        # if it's a special, remember that  
+        if char in special_characters:  
+            has_special = True  
+        # if uppercase, mark it  
+        if char.isupper():  
+            has_upper = True  
+        # if lowercase, mark it  
+        if char.islower():  
+            has_lower = True  
+  
+    #if length >= 8 then points += 1  
+    if len(password) >= 8:  # long enough  
+        points += 1  
+    #if has_num == True then points += 1  
+    if has_num:  # saw a number  
+        points += 1  
+    #if has_special == True then points += 1  
+    if has_special:  # saw a special char  
+        points += 1  
+    #if has_upper == True then points += 1  
+    if has_upper:  # saw uppercase  
+        points += 1  
+    #if has_lower == True then points += 1  
+    if has_lower:  # saw lowercase  
+        points += 1  
+  
+    #Meeting the length requirement == True: +1 point  
+    #Containing uppercase letters == True: +1 point  
+    #Containing lowercase letters == True: +1 point  
+    #Containing numbers == True: +1 point  
+    #Containing special characters == True: +1 point  
+  
+    #if points == 1  
+    if points == 1 or points == 2:  
+        print("password is weak \n")  # say weak for 1 or 2  
+        print(f"{points} point(s)")  
+    #display password is medium  
+    elif points == 3:  
+        print("password is medium\n")  # 3 gets medium  
+    #display password is good  
+    elif points == 4:  
+        print("password is good \n")  # 4 gets good  
+    #display password is strong  
+    elif points == 5:  
+        print("password is strong \n")  # all 5, very strong  
+    else:   
+        print("please try again")  # no points, something went wrong  
+  
+    #loop repeats, can check another password  
