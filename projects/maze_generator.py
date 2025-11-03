c9eea3cfc9e446a80ef1e43ae810d3a832a3f9dc
INTELION11 <jacob.quezada@ucas-edu.net>
2025-11-03 09:53:16 -0700

    turtles maze

--- projects/maze_generator.py
+++ projects/maze_generator.py
@@ -1,5 +1,8 @@
 # JQ 1st maze generator
 import turtle as t
+import random as r
+
+s = 50
 t.Turtle()
 t.shape("turtle")
 t.color("white")
@@ -7,6 +10,33 @@ screen = t.Screen()
 screen.bgcolor("black")
 t.penup()
 t.goto(-300,300)
+t.forward(50)
+t.pendown()
+t.forward(250)
+t.right(90)
+t.forward(300)
+t.right(90)
+t.penup()
+t.forward(50)
+t.pendown()
+t.forward(250)
+t.right(90)
+t.forward(300)
+t.right(90)
+t.penup()
+t.forward(50)
+t.pendown()
+t.right(90)
+
+spaces = [[-300,300]]
+r.randint(1,6)
+# x coordinate and y coordinate
+x = r.randint(1,5)
+y = r.randint(1,5)
+#size of the line
+
+
+spaces.append([x,y])
 t.done()
 
 
