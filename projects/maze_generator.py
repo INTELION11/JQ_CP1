caa593c90d6a7b28fe428f9fecbb1ce1589ebbb1
INTELION11 <jacob.quezada@ucas-edu.net>
2025-11-04 09:53:12 -0700

    maze

--- projects/maze_generator.py
+++ projects/maze_generator.py
@@ -1,7 +1,8 @@
 # JQ 1st maze generator
 import turtle as t
 import random as r
-
+column = True
+row = True
 s = 50
 t.Turtle()
 t.shape("turtle")
@@ -27,16 +28,59 @@ t.penup()
 t.forward(50)
 t.pendown()
 t.right(90)
+t.penup()
+counter = 0
+t.goto(-300,250)
+t.right(-90)
+def rand_num():
+    return r.randint(1,2)
 
-spaces = [[-300,300]]
-r.randint(1,6)
-# x coordinate and y coordinate
-x = r.randint(1,5)
-y = r.randint(1,5)
-#size of the line
+while column == True:
+    c1 = rand_num()
+    c2 = rand_num()
+    c3 = rand_num()
+    c4 = rand_num()
+    c5 = rand_num()
+    c6 = rand_num()
+    
+    
+    grid_columns = [c1,c2,c3,c4,c5,c6]
+    for col in grid_columns:
+        if col == 1:
+            t.penup()
+            t.forward(50)
+            t.pendown()
+            t.right(-90)
+            t.forward(50)
+            t.right(180)
+            t.forward(50)
+            t.right(-90)
+        else:
+            t.penup()
+            t.forward(50)
+            t.pendown()
+        if t.xcor(0):
+            t.right(180)
+            t.forward(250)
+            t.right(-90)
+            t.forward(50)
+            t.right(-90)
+        else:
+            counter = 0
+
+   
+
+while row == True:
+    l1 = r.randint(1,2)
+    l2 = rand_num()
+    l3 = rand_num()
+    l4 = rand_num()
+    l5 = rand_num()
+    l6 = rand_num()
+    grid_rows = [l1,l2,l3,l4,l5,l6]
+    row = False
 
 
-spaces.append([x,y])
 t.done()
 
 
