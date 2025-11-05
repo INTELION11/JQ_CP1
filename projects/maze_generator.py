92522f0e049a94e31b5b14986b7fa7ff31560db2
INTELION11 <jacob.quezada@ucas-edu.net>
2025-11-05 09:53:08 -0700

    half maze

--- projects/maze_generator.py
+++ projects/maze_generator.py
@@ -2,7 +2,7 @@
 import turtle as t
 import random as r
 column = True
-row = False
+row = True
 s = 50
 t.Turtle()
 t.shape("turtle")
@@ -10,19 +10,19 @@ t.color("white")
 screen = t.Screen()
 screen.bgcolor("black")
 t.penup()
-t.goto(-300,300)
+t.goto(-500,500)
 t.forward(50)
 t.pendown()
-t.forward(250)
+t.forward(500)
 t.right(90)
-t.forward(300)
+t.forward(500)
 t.right(90)
 t.penup()
 t.forward(50)
 t.pendown()
-t.forward(250)
+t.forward(500)
 t.right(90)
-t.forward(300)
+t.forward(500)
 t.right(90)
 t.penup()
 t.forward(50)
@@ -30,11 +30,11 @@ t.pendown()
 t.right(90)
 t.penup()
 counter = 0
-t.goto(-300,250)
+t.goto(-500,450)
 t.right(-90)
 def rand_num():
     return r.randint(1,2)
-
+t.speed(0)
 while column == True:
     c1 = rand_num()
     c2 = rand_num()
@@ -42,10 +42,25 @@ while column == True:
     c4 = rand_num()
     c5 = rand_num()
     c6 = rand_num()
+    c7 = rand_num()
+    c8 = rand_num()
+    c9 = rand_num()
+    c10 = rand_num()
+
     
-    
-    grid_columns = [c1,c2,c3,c4,c5,c6]
+   
+    grid_columns = [c1,c2,c3,c4,c5,c6,c7,c8,c9,c10]
     for col in grid_columns:
+
+        if t.xcor() >= 0:
+            t.penup()
+            t.right(180)
+            t.forward(500)
+            t.left(90)
+            t.forward(50)
+            t.left(90)
+        if t.ycor() <= 0:
+            column = False
         if col == 1:
             t.penup()
             t.forward(50)
@@ -55,19 +70,15 @@ while column == True:
             t.right(180)
             t.forward(50)
             t.right(-90)
-        else:
+            counter += 1
+        elif col == 2:
             t.penup()
             t.forward(50)
             t.pendown()
-        if t.xcor() == 0:
-            t.right(180)
-            t.forward(250)
-            t.right(-90)
-            t.forward(50)
-            t.right(-90)
-        else:
-            print("something went wrong")
-
+            counter += 1
+t.penup()
+t.goto(-500,450)
+t.pendown()
    
 
 while row == True:
@@ -77,8 +88,40 @@ while row == True:
     l4 = rand_num()
     l5 = rand_num()
     l6 = rand_num()
-    grid_rows = [l1,l2,l3,l4,l5,l6]
-    row = False
+    l7 = rand_num()
+    l8 = rand_num()
+    l9 = rand_num()
+    l10 = rand_num()
+    grid_rows = [l1,l2,l3,l4,l5,l6,l7,l8,l9,l10]
+    for row in grid_rows:
+
+        if t.xcor() >= 0:
+            t.penup()
+            t.right(180)
+            t.forward(500)
+            t.left(90)
+            t.forward(50)
+            t.left(90)
+        if t.ycor() <= 0:
+            row = False
+        if row == 1:
+            t.penup()
+            t.forward(50)
+            t.pendown()
+            t.right(-90)
+            t.forward(50)
+            t.right(180)
+            t.forward(50)
+            t.right(-90)
+            counter += 1
+        elif row == 2:
+            t.penup()
+            t.forward(50)
+            t.pendown()
+            counter += 1
+
+
+
 
 
 t.done()
