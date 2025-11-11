f18c3ff5db5137d0fec304548684d61ce3f63b26
INTELION11 <jacob.quezada@ucas-edu.net>
2025-11-11 09:53:25 -0700

    order up

---
+++ projects/order.py
@@ -0,0 +1,25 @@
+# JQ 1st Order up
+meal = {
+    "Cheeseburger": 5.00,
+    "Bacon burger": 6.00,
+    "Double-Double":3.00,
+    "ShackBurger": 12.00,
+    "ButterBurger": 4.00
+}
+drink = {
+    "lemonade": 2.50,
+    "milkshake": 2.50,
+    "soda": 3.00,
+    "tap water": 2.00,
+    "orange juice": 4.00
+}
+side_dishes = {
+    "American fries": 1.00,
+    "mashed potatoes": 3.00,
+    "bread": 3.00,
+    "onion rings": 2.50,
+    "mac and cheese": 12.00
+}
+action = input(" Welcome to [insert name] Restaurant, pleaese type quit at any time to quit")
+
+if action == 
