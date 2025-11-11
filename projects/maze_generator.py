02c2c6f223616dcf14dc27ca9c0d67f2b30df4c2
INTELION11 <jacob.quezada@ucas-edu.net>
2025-11-11 04:36:34 +0000

    finito

--- projects/maze_generator.py
+++ projects/maze_generator.py
@@ -1,177 +1,114 @@
-# JQ 1st maze generator
-import turtle as t
-import random as r
-import time
-column = True
-row = False
-row_list = [[],[],[],[],[],[],[],[],[],[]]
-column_list = [[],[],[],[],[],[],[],[],[],[]]
-r_sublist_index = 0
-c_sublist_index = -3
-s = 50
-t.Turtle()
-t.shape("turtle")
-t.color("white")
-screen = t.Screen()
-screen.bgcolor("black")
-#screen.tracer(0)
-t.penup()
-t.goto(-500,500)
-t.forward(50)
-t.pendown()
-t.forward(500)
-t.right(90)
-t.forward(500)
-t.right(90)
-t.penup()
-t.forward(50)
-t.pendown()
-t.forward(500)
-t.right(90)
-t.forward(500)
-t.right(90)
-t.penup()
-t.forward(50)
-t.pendown()
-t.right(90)
-t.penup()
-counter = 0
-t.goto(-500,450)
-t.right(-90)
-def rand_num():
-    return r.randint(1,2)
-t.speed(0)
-while column == True:
-    c1 = rand_num()
-    c2 = rand_num()
-    c3 = rand_num()
-    c4 = rand_num()
-    c5 = rand_num()
-    c6 = rand_num()
-    c7 = rand_num()
-    c8 = rand_num()
-    c9 = rand_num()
-    c10 = rand_num()
-    row_list[r_sublist_index].append(c1)
-    row_list[r_sublist_index].append(c2)
-    row_list[r_sublist_index].append(c3)
-    row_list[r_sublist_index].append(c4)
-    row_list[r_sublist_index].append(c5)
-    row_list[r_sublist_index].append(c6)
-    row_list[r_sublist_index].append(c7)
-    row_list[r_sublist_index].append(c8)
-    row_list[r_sublist_index].append(c9)
-    row_list[r_sublist_index].append(c10)
-    r_sublist_index += 1
+# JQ 1st maze generator   
+import turtle as t    
+import random as r  
+# make maze_solvable False    
+maze_solvable = False    
+def is_maze_solvable(maze):    
+    # 1 = wall, 2 = open path    
+    visited = [[False for _ in range(10)] for _ in range(10)]    
+    if maze[0][0] == 1 or maze[9][9] == 1:    
+        return False    
+    queue = [(0, 0)]    
+    visited[0][0] = True    
+    while queue:    
+        x, y = queue.pop(0)    
+        if (x, y) == (9, 9):    
+            return True    
+        for dx, dy in [(-1,0),(1,0),(0,-1),(0,1)]:    
+            nx, ny = x+dx, y+dy    
+            if 0 <= nx < 10 and 0 <= ny < 10:    
+                if not visited[nx][ny] and maze[nx][ny] == 2:    
+                    visited[nx][ny] = True    
+                    queue.append((nx, ny))    
+    return False    
+
+# While maze_solvable is False  
+while maze_solvable == False:  
+    # Make blank row_list with 10 lists  
+    row_list = [[],[],[],[],[],[],[],[],[],[]]  
+    # For i in range(10)  
+    for i in range(10):  
+        # For j in range(10)  
+        for j in range(10):  
+            # Add random 1 or 2 to row_list  
+            row_list[i].append(r.randint(1,2))  
+    # If is_maze_solvable(row_list) is True  
+    if is_maze_solvable(row_list) == True:  
+        # maze_solvable True  
+        maze_solvable = True  
    
-    grid_columns = [c1,c2,c3,c4,c5,c6,c7,c8,c9,c10]
-    for col in grid_columns:
-
-        if t.xcor() >= 0:
-            t.penup()
-            t.right(180)
-            t.forward(500)
-            t.left(90)
-            t.forward(50)
-            t.left(90)
-        if t.ycor() <= 0:
-            column = False
-        if col == 1:
-            t.penup()
-            t.forward(50)
-            t.pendown()
-            t.right(-90)
-            t.forward(50)
-            t.right(180)
-            t.forward(50)
-            t.right(-90)
-            counter += 1
-        elif col == 2:
-            t.penup()
-            t.forward(50)
-            t.pendown()
-            counter += 1
-t.penup()
-t.goto(-500,500)
-t.speed(0)
-t.pendown()
-row = True
-   
-#
-while row == True:
-    l1 = r.randint(1,2)
-    l2 = rand_num()
-    l3 = rand_num()
-    l4 = rand_num()
-    l5 = rand_num()
-    l6 = rand_num()
-    l7 = rand_num()
-    l8 = rand_num()
-    l9 = rand_num()
-    l10 = rand_num()
-    column_list[c_sublist_index].append(l1)
-    column_list[c_sublist_index].append(l2)
-    column_list[c_sublist_index].append(l3)
-    column_list[c_sublist_index].append(l4)
-    column_list[c_sublist_index].append(l5)
-    column_list[c_sublist_index].append(l6)
-    column_list[c_sublist_index].append(l7)
-    column_list[c_sublist_index].append(l8)
-    column_list[c_sublist_index].append(l9)
-    column_list[c_sublist_index].append(l10)
-    c_sublist_index += 1
-    grid_rows = [l1,l2,l3,l4,l5,l6,l7,l8,l9,]
-    for rows in grid_rows:
-
-        if t.ycor() <= 0:
-            t.penup()
-            t.setheading(90)
-            t.forward(500)
-            t.setheading(0)
-            t.forward(50)
-            t.setheading(270)
-        if t.xcor() >= 0:
-            row = False
-        if rows == 1:
-            t.setheading(270)
-            t.penup()
-            t.forward(50)
-            if t.ycor() <= 0:
-                t.penup()
-                t.setheading(90)
-                t.forward(500)
-                t.setheading(0)
-                t.forward(50)
-                t.setheading(270)
-            t.pendown()
-            t.setheading(0)
-            t.forward(50)
-            t.right(180)
-            t.forward(50)
-            t.setheading(270)
-            counter += 1
-        elif rows == 2:
-            t.setheading(270)
-            t.penup()
-            t.forward(50)
-            if t.ycor() <= 0:
-                t.penup()
-                t.setheading(90)
-                t.forward(500)
-                t.setheading(0)
-                t.forward(50)
-                t.setheading(270)
-            t.pendown()
-            counter += 1
+  
+# draw maze with turtle    
+# set turtle top left    
+# for i in range(10)    
+for i in range(10):    
+    # for j in range(10)    
+    for j in range(10):    
+        # if its 1 draw white square    
+        if row_list[i][j] == 1:    
+            # draw filled square    
+            pass    
+        # else just move    
+  
+# make is_maze_solvable(maze)    
+    # make visited grid 10x10 all False    
+    # if maze[0][0] is 1 or maze[9][9] is 1 return False    
+    # queue = (0,0)    
+    # while queue not empty    
+        # pop x,y from queue    
+        # if x is 9 and y is 9 return True    
+        # check all directions    
+        # if next cell in bounds, not visited, and open, add to queue    
+    # return False    
+  
 
 
-t.clear()
-
-#screen.tracer(1)
-
-   
-    
-t.done()
-
-
-# go randomly inside until it touches a line then undo
+  
+# Make maze_solvable False    
+maze_solvable = False    
+  
+# While maze_solvable is False    
+while maze_solvable == False:    
+    # Make a blank row_list with 10 lists    
+    row_list = [[],[],[],[],[],[],[],[],[],[]]    
+    # For i in range(10):    
+    for i in range(10):    
+        # For j in range(10):    
+        for j in range(10):    
+            # Append random 1 or 2    
+            row_list[i].append(r.randint(1,2))    
+    # If maze is solvable    
+    if is_maze_solvable(row_list):    
+        maze_solvable = True    
+  
+# Draw maze with turtle    
+t.Turtle()    
+t.shape("turtle")    
+t.color("white")    
+screen = t.Screen()    
+screen.bgcolor("black")    
+t.speed(0)    
+t.penup()    
+t.goto(-500, 450)    
+t.setheading(0)    
+t.pendown()    
+  
+# For i in range(10):    
+for i in range(10):    
+    # For j in range(10):    
+    for j in range(10):    
+        t.penup()    
+        t.goto(-500 + 50*j, 450 - 50*i)    
+        # If row_list[i][j] == 1:    
+        if row_list[i][j] == 1:    
+            t.pendown()    
+            t.fillcolor("white")    
+            t.begin_fill()    
+            for _ in range(4):    
+                t.forward(50)    
+                t.right(90)    
+            t.end_fill()    
+            t.penup()    
+        # Else: just move, don't draw    
+t.hideturtle()    
+t.done()    
