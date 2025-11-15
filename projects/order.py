67d749ab6f92842eca72ad65f2be19646cc1de25
INTELION11 <jacob.quezada@ucas-edu.net>
2025-11-15 02:29:57 +0000

    ultimo_finito

--- projects/order.py
+++ projects/order.py
@@ -1,17 +1,24 @@
 # JQ 1st Order up
+# meal,drink,side checkers is true
 meal_check = True   
 drink_check = True
 Side_check = True
+# def check(items ordered) 
+# total is sum of items
+#for items in dictionary order
+# print items and price
+# print total
 def check(ordered):
     total = sum(ordered.values())
     for items in ordered:
           print(f"{items} :::: {ordered[items]}")
          # total += order[items]
     print(f"your total is {total}")
-     
+    # dictionary order is {}
 order = {
  
 }
+# meal is burgers: price,
 meal = {
     "cheeseburger": 5.00,
     "bacon burger": 6.00,
@@ -19,6 +26,7 @@ meal = {
     "shackburger": 12.00,
     "butterburger": 4.00
 }
+#drinks is drink:price,
 drink = {
     "lemonade": 2.50,
     "milkshake": 2.50,
@@ -26,6 +34,7 @@ drink = {
     "tap water": 2.00,
     "orange juice": 4.00
 }
+# side dishes is side:price,
 side_dishes = {
     "american fries": 1.00,
     "mashed potatoes": 3.00,
@@ -33,13 +42,21 @@ side_dishes = {
     "onion rings": 2.50,
     "mac and cheese": 12.00
 }
+# display welcome to the restaurnat you can press quit at any time
 print(" Welcome to [insert name] Restaurant, pleaese type quit at any time to quit, what would you like to")
-
+# meal check while loop
 while meal_check == True:
+    # display menu
     for key in meal.keys():
         print(f"{key} is {meal[key]}")
     print("")
+    # ask user what item to buy
+     # if answer is quit, quit
+    # check if item is in menue
+    #add meal and price to order dictionary
+    # meal check is false
     action = input("what meal would you like\n ").strip().lower()
+    
     if action == "quit":
         quit()
     if action not in meal:
@@ -50,12 +67,16 @@ while meal_check == True:
         print(order)
         meal_check = False
     print("")
-
+ # display menu
 while drink_check == True:
     for key in drink.keys():
             print(f"{key} is {drink[key]}")
     print("")
-    
+        # ask user what item to buy
+     # if answer is quit, quit
+    # check if item is in menue
+    #add meal and price to order dictionary
+    # meal check is false
     action = input("what drink would you like\n ").strip().lower()
     if action == "quit":
         quit()
@@ -67,12 +88,16 @@ while drink_check == True:
         print(order)
         drink_check = False
     
-    
+  # display menu   
 while Side_check == True:
     for key in side_dishes.keys():
             print(f"{key} is {side_dishes[key]}")
     print("")
-
+    # ask user what item to buy
+     # if answer is quit, quit
+    # check if item is in menue
+    #add meal and price to order dictionary
+    # meal check is false
     action = input("what side dish would you like\n ").strip().lower()
     if action == "quit":
         quit()
@@ -83,12 +108,17 @@ while Side_check == True:
         order[f"{action}"] = side_dishes[action]
         print(order)
         Side_check = False
+         # display menu
 Side_check = True
 while Side_check == True:
     for key in side_dishes.keys():
             print(f"{key} is {side_dishes[key]}")
     print("")
-
+    # ask user what item to buy
+     # if answer is quit, quit
+    # check if item is in menue
+    #add meal and price to order dictionary
+    # meal check is false
     action = input("what side dish would you like\n ").strip().lower()
     if action == "quit":
         quit()
@@ -100,7 +130,6 @@ while Side_check == True:
          continue
     else:
         order[f"{action}"] = side_dishes[action]
-        print(order)
         Side_check = False
-
+# call check function
 check(order)
