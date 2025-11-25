72a9612560a8f5ad676838ad19db7247ffad5b3a
INTELION11 <jacob.quezada@ucas-edu.net>
2025-11-25 01:43:58 +0000

    lo

--- projects/partener_factorials.py
+++ projects/partener_factorials.py
@@ -1,33 +1,36 @@
-#JQ 1st Factorials
-#spheudo code writter - isabella
-#def function_factorial():
-def function_factorial():
-#asking user what number they want 
-    user_num = input(" What number do you want?")
-# cheking if its a number -
-# Jacob - I chose to use try and exept for cheking if its a number
-    try:
-        # i had to use int isntead of float so that it was an intiger
-        int(user_num)
-    except ValueError:
-        print("Please try again")
-#              ()float
-# If number is a real number
-    if user_num is int:
-#     display (Wow you got a number!)
-        print("wow you got a real number")
-    else:
-#else:
-        print("PLEASE try again")
-        continue
- #  display(try again) Send user back up
- #      list()
-    num_list = []
-#Getting all the numbers under using -1
-    counter = 0
-    counter += 1
-    num_list.append(user_num - counter)
-# Putting the numbers in a list
-#multiplying them all together
-# Print first number
-#print factored number
+# JQ 1st Factorials  
+# spheudo code writter - isabella  
+  
+def function_factorial():  
+    # asking user what number they want   
+    user_num = input("What number do you want? ")  
+  
+    # checking if it's a number  
+    try:  
+        user_num = int(user_num)  # must be integer for factorial  
+    except ValueError:  
+        print("Please try again")  
+        return  # stop function if not a number  
+  
+    # If number is an integer  
+    print("wow you got a real number")  
+  
+    # list to collect numbers  
+    num_list = []  
+  
+    # Getting all the numbers under using -1  
+    for i in range(user_num, 0, -1):  
+        num_list.append(i)  
+  
+    # multiplying them all together for the factorial  
+    factored_number = 1  
+    for num in num_list:  
+        factored_number *= num  
+  
+    # Print first number  
+    print(f"First number: {user_num}")  
+    # print factored number  
+    print(f"Factorial: {factored_number}")  
+  
+# Run the function  
+function_factorial()  
