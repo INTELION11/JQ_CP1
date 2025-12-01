6e6c3ce40680eebb4fef52c2a3bed902de8493ec
INTELION11 <jacob.quezada@ucas-edu.net>
2025-12-01 09:51:44 -0700

    starter area

---
+++ projects/Final_Pseudocode.py
@@ -0,0 +1,23 @@
+# JQ 1st Final Project pseudocode
+# import random as r
+# display (f"\033[38;2;160;160;160mWelcome adventurer, your name is lyte\033[1m")
+#STR = r.randint(1-20)
+#DEX = r.randint(1-20)
+#CON = r.randint(1-20)
+#INT = r.randint(1-20)
+#WIS = r.randint(1-20)
+#CHA = r.randint(1-20)
+# stat_opinion = display input f"here are your stats: {STR DEX CON INT WIS CHA}, tyou have one more chance to re roll, say yes if you want to reroll, say no if you dont"
+# if stat_opinion == yes
+    #STR = r.randint(1-20)
+    #DEX = r.randint(1-20)
+    #CON = r.randint(1-20)
+    #INT = r.randint(1-20)
+    #WIS = r.randint(1-20)
+    #CHA = r.randint(1-20)
+# elif stat_opinion == no
+    #display ""
+# else
+    #display""
+# display " As youre walking around the beoutiful world of janirate, you see a globe and then boom, flash your teleported across dimensions, you meet the final boss, he reveals his plan and seends you into oblivion, into the desrt of samurabi "
+# def desert_samurabii(STR #DEX #CON #INT #WIS #CHA)
