68de2d5d28b11c9eb899640390134b72c5fc6773
INTELION11 <jacob.quezada@ucas-edu.net>
2025-12-02 09:53:12 -0700

    f3 maps finito

--- projects/Final_Pseudocode.py
+++ projects/Final_Pseudocode.py
@@ -1,13 +1,16 @@
 # JQ 1st Final Project pseudocode
-# import random as r
+# import random
+#import time
+# inventory is [] - all items that can be used in hand are stored here for atack and healing
 # display (f"\033[38;2;160;160;160mWelcome adventurer, your name is lyte\033[1m")
-#STR = r.randint(1-20)
+#STR = r.randint(1-20) - random stats
 #DEX = r.randint(1-20)
 #CON = r.randint(1-20)
 #INT = r.randint(1-20)
 #WIS = r.randint(1-20)
 #CHA = r.randint(1-20)
-# stat_opinion = display input f"here are your stats: {STR DEX CON INT WIS CHA}, tyou have one more chance to re roll, say yes if you want to reroll, say no if you dont"
+#HP = r.randint(50-70)
+# stat_opinion = display input f"here are your stats: {STR DEX CON INT WIS CHA HP}, tyou have one more chance to re roll, say yes if you want to reroll, say no if you dont"
 # if stat_opinion == yes
     #STR = r.randint(1-20)
     #DEX = r.randint(1-20)
@@ -15,9 +18,33 @@
     #INT = r.randint(1-20)
     #WIS = r.randint(1-20)
     #CHA = r.randint(1-20)
+    #HP = r.randint(50-70)
+    # display f"here are your stats {STR DEX CON INT WIS CHA HP}""
 # elif stat_opinion == no
     #display ""
 # else
     #display""
 # display " As youre walking around the beoutiful world of janirate, you see a globe and then boom, flash your teleported across dimensions, you meet the final boss, he reveals his plan and seends you into oblivion, into the desrt of samurabi "
-# def desert_samurabii(STR #DEX #CON #INT #WIS #CHA)
+#    def desert_samurabii(STR #DEX #CON #INT #WIS #CHA, HP #inventory)
+#    you hit the desert of hamurabi after being teleported by cronos
+#    action display input " you wake up in the desert's scorching heat, what do you do, look around, rest,"
+#    if action is rest 
+#       display "after a short while you feel the ground rumbling when a giant desert sandworm appears"
+#   else: 
+#       display "after looking around you find nothing but more desert, when suddenly teh ground starts shaking"
+#   mon_hp = 34
+#    mon_dex = 13
+#   mon_weapon = body
+#combat(mon_hp,mon_dex,mon_weapon,STR #DEX #CON #INT #WIS #CHA, HP #inventory)- player and montser fight to death, exaustion score increases every 2 turns
+#if monster = is dead
+#   inventory.append(meat,gold,time_cristal)
+#   display ( inventory updates: {inventory})
+#if player is dead loop universe
+# action display input "do you want to use the warp cristal?"
+#   if action is yes 
+#   crastal_option is display input"do you want to go though portal 1 or 2"
+#if cristal_option is 1
+#   def Ötzi’s Tusdra(STR #DEX #CON #INT #WIS #CHA, HP #inventory) - a cooking game that is time based
+# elif cristal_option is 1
+#   def The iztec Jungla(STR #DEX #CON #INT #WIS #CHA, HP #inventory) - runs a combat situation like in the desert of hamurabi
+#   
