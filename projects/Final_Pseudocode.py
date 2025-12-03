735a72d93fdb95ea881e3c10b367ba0661bf2efe
INTELION11 <jacob.quezada@ucas-edu.net>
2025-12-03 09:52:52 -0700

    more detalle

--- projects/Final_Pseudocode.py
+++ projects/Final_Pseudocode.py
@@ -1,50 +1,67 @@
 # JQ 1st Final Project pseudocode
+#variables: #STR #DEX  #CON #INT #WIS #CHA #HP, action, inventory, monster stats, been_to_worlds, worlds_not_been_to,
 # import random
 #import time
-# inventory is [] - all items that can be used in hand are stored here for atack and healing
-# display (f"\033[38;2;160;160;160mWelcome adventurer, your name is lyte\033[1m")
-#STR = r.randint(1-20) - random stats
-#DEX = r.randint(1-20)
-#CON = r.randint(1-20)
-#INT = r.randint(1-20)
-#WIS = r.randint(1-20)
-#CHA = r.randint(1-20)
-#HP = r.randint(50-70)
+# create inventory as a list, all items that can be used in hand are stored here for atack and healing
+# (f"\033[38;2;160;160;160mWelcome adventurer, your name is lyte\033[1m") - make the text color light grey
+# randomize     #STR #DEX  #CON #INT #WIS #CHA 1-20 and #HP 60-70
 # stat_opinion = display input f"here are your stats: {STR DEX CON INT WIS CHA HP}, tyou have one more chance to re roll, say yes if you want to reroll, say no if you dont"
 # if stat_opinion == yes
-    #STR = r.randint(1-20)
-    #DEX = r.randint(1-20)
-    #CON = r.randint(1-20)
-    #INT = r.randint(1-20)
-    #WIS = r.randint(1-20)
-    #CHA = r.randint(1-20)
-    #HP = r.randint(50-70)
+# randomize     #STR #DEX  #CON #INT #WIS #CHA 1-20 and #HP 60-70
     # display f"here are your stats {STR DEX CON INT WIS CHA HP}""
 # elif stat_opinion == no
     #display ""
 # else
     #display""
-# display " As youre walking around the beoutiful world of janirate, you see a globe and then boom, flash your teleported across dimensions, you meet the final boss, he reveals his plan and seends you into oblivion, into the desrt of samurabi "
-#    def desert_samurabii(STR #DEX #CON #INT #WIS #CHA, HP #inventory)
-#    you hit the desert of hamurabi after being teleported by cronos
-#    action display input " you wake up in the desert's scorching heat, what do you do, look around, rest,"
-#    if action is rest 
-#       display "after a short while you feel the ground rumbling when a giant desert sandworm appears"
-#   else: 
-#       display "after looking around you find nothing but more desert, when suddenly teh ground starts shaking"
-#   mon_hp = 34
-#    mon_dex = 13
-#   mon_weapon = body
-#combat(mon_hp,mon_dex,mon_weapon,STR #DEX #CON #INT #WIS #CHA, HP #inventory)- player and montser fight to death, exaustion score increases every 2 turns
-#if monster = is dead
-#   inventory.append(meat,gold,time_cristal)
-#   display ( inventory updates: {inventory})
-#if player is dead loop universe
-# action display input "do you want to use the warp cristal?"
-#   if action is yes 
-#   crastal_option is display input"do you want to go though portal 1 or 2"
-#if cristal_option is 1
-#   def Ötzi’s Tusdra(STR #DEX #CON #INT #WIS #CHA, HP #inventory) - a cooking game that is time based
-# elif cristal_option is 1
-#   def The iztec Jungla(STR #DEX #CON #INT #WIS #CHA, HP #inventory) - runs a combat situation like in the desert of hamurabi
-#   
+# display " Cut scene "
+"""-------------------------------------------------------------------------------------------------------------------------------"""
+#    define desert_hamurabii,The iztec Jungla,mlɒɘЯbɘqɿɒWɘ⑁TTheWarpedrealm,Pangeon,Modern world-(choose to fight or wait 2:30 minutes to be teleported anywhere else)  (STR #DEX #CON #INT #WIS #CHA, HP #inventory)
+    #   display cutscene(descriptive dialouge of the place)
+    #   mon_hp = 34(example)
+    #    mon_dex = 13(example)
+    #   mon_weapon = body (example)
+    #combat(mon_hp,mon_dex,mon_weapon,STR #DEX #CON #INT #WIS #CHA, HP #inventory)- player and montser fight to death, exaustion score for- desert, basic combat function created in a nother project with sligt diferenciations like multiple monsters, and added modifiers score increases every 2, oxygen poisoning.to use on diferent maps
+    #if monster = is dead
+    #   add to inventory (meat,gold,time_cristal, ancient artifact(depends on the map))
+    #   display ( inventory updates: {inventory}
+    # open shop keeper, dialouge and display items to buy from him, exit function, made using the grocery list from another project
+    #if the player is dead loop universe to the start
+    #   crastal_option is "do you want to go though portal 1 or 2"
+    #if 1
+    #  option one is random between 1-8,  the number decides what world they teleport into apart from the world they have been to
+    # if 2 go to final boss cronos's world
+"""-------------------------------------------------------------------------------------------------------------------------------"""
+# define Final Boss
+    # cronos dialouge dialouge 
+    #combat(mon_hp,mon_dex,mon_weapon,STR #DEX #CON #INT #WIS #CHA, HP #inventory)- player and montser fight to death, the final boss cronos can attack 6 times as a hexaweilder, for every map that you have been to he looses one of his 6 arms, he also looses a turn
+    #if you defeat him you win
+    # if you loose sad dialouge
+"""-------------------------------------------------------------------------------------------------------------------------------"""
+# define Ötzi’s Tusdra
+    # dialouge
+    # minigame of cooking:
+    # a stop watch will start and you will have to quickly say N or Y if an item is food, based on howmany items you got on how many seconds it will get an avarage and grade you on that, if you answer incorrectly you slip and loose a point
+    # bad avarage you gain a hat with curse of binding that removes -2 charisma
+    # if medium you gain slippers removes the intimidation effect from you
+    # if good you gain (ice ring to cast spells)
+"""-------------------------------------------------------------------------------------------------------------------------------"""
+# Items 
+# ice ring to cast spells
+# slippers removes the intimidation effect from you
+# a hat with curse of binding that removes -2 charisma
+# gold
+# ability : Gorilla warfare
+# The Rapier of sacrifice ( Stab yourself stabs your enemy, stabbing your enemy stabs yourself)
+# Graviole wand ( permanent +2 dexterity bonus) (force attack)
+# Meat
+# intimidated spell
+# Cyber chip ( grants +2 intelligence permanently)
+# cronos ‘s ability is to turn back one turn in time
+# Bootle of XP (updates player stats +2)
+# Great sword (3d8)( strength, higher chance of missing)
+# Rapier (Dexterity weapon)(1d8)
+# Wand ability to cast “start shooter”( blinds enemy and deals 1d6 damage)
+# Armor +5 AC 
+# sword
+"""-------------------------------------------------------------------------------------------------------------------------------"""
+
