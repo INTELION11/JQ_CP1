6253ee55560ff1ce38b2c959455fdcb5f3c57195
INTELION11 <jacob.quezada@ucas-edu.net>
2025-12-04 09:52:31 -0700

    finito pseudo

--- projects/Final_Pseudocode.py
+++ projects/Final_Pseudocode.py
@@ -1,26 +1,26 @@
 # JQ 1st Final Project pseudocode
-#variables: #STR #DEX  #CON #INT #WIS #CHA #HP, action, inventory, monster stats, been_to_worlds, worlds_not_been_to,
+#Important variables: #STR(if they hit or miss an attack, added to their roll) #DEX(if they dodge an attack with the roll item and adds to AC)  #HP, action, inventory, monster stats, been_to_worlds, worlds_not_been_to,
 # import random
 #import time
 # create inventory as a list, all items that can be used in hand are stored here for atack and healing
 # (f"\033[38;2;160;160;160mWelcome adventurer, your name is lyte\033[1m") - make the text color light grey
-# randomize     #STR #DEX  #CON #INT #WIS #CHA 1-20 and #HP 60-70
-# stat_opinion = display input f"here are your stats: {STR DEX CON INT WIS CHA HP}, tyou have one more chance to re roll, say yes if you want to reroll, say no if you dont"
+# randomize     #STR #DEX 1-20 and #HP 60-70
+# stat_opinion = display input f"here are your stats: {STR DEX HP}, tyou have one more chance to re roll, say yes if you want to reroll, say no if you dont"
 # if stat_opinion == yes
-# randomize     #STR #DEX  #CON #INT #WIS #CHA 1-20 and #HP 60-70
-    # display f"here are your stats {STR DEX CON INT WIS CHA HP}""
+# randomize     #STR #DEX 1-20 and #HP 60-70
+    # display f"here are your stats {STR DEX HP}""
 # elif stat_opinion == no
     #display ""
 # else
     #display""
 # display " Cut scene "
 """-------------------------------------------------------------------------------------------------------------------------------"""
-#    define desert_hamurabii,The iztec Jungla,mlɒɘЯbɘqɿɒWɘ⑁TTheWarpedrealm,Pangeon,Modern world-(choose to fight or wait 2:30 minutes to be teleported anywhere else)  (STR #DEX #CON #INT #WIS #CHA, HP #inventory)
-    #   display cutscene(descriptive dialouge of the place)
+#    define desert_hamurabii,The iztec Jungla,mlɒɘЯbɘqɿɒWɘ⑁TTheWarpedrealm,Pangeon,Modern world,Cybercity 3012,Medieval Europe -(choose to fight or wait 2:30 minutes to be teleported anywhere else -modern world)  (STR #DEX  #INT #WIS #CHA, HP #inventory)
+    #   display cutscene (descriptive dialouge of the place)
     #   mon_hp = 34(example)
     #    mon_dex = 13(example)
     #   mon_weapon = body (example)
-    #combat(mon_hp,mon_dex,mon_weapon,STR #DEX #CON #INT #WIS #CHA, HP #inventory)- player and montser fight to death, exaustion score for- desert, basic combat function created in a nother project with sligt diferenciations like multiple monsters, and added modifiers score increases every 2, oxygen poisoning.to use on diferent maps
+    #combat(Inventory,mon_hp,mon_dex,mon_weapon,STR #DEX, HP #inventory)- player and montser fight to death,(add Weapons and armor modifiers) (exaustion score for- desert), basic combat function created in a nother project with sligt diferenciations (multiple monsters and -4 inteligence- Cybercity 3012), (every 2 turns, oxygen poisoning 1d4 - Pangeon)(+4 dex-TheWarpedrealm)(Intimadated(Disadvantage) and -4 STR- Modern world).to use on diferent maps
     #if monster = is dead
     #   add to inventory (meat,gold,time_cristal, ancient artifact(depends on the map))
     #   display ( inventory updates: {inventory}
@@ -33,7 +33,7 @@
 """-------------------------------------------------------------------------------------------------------------------------------"""
 # define Final Boss
     # cronos dialouge dialouge 
-    #combat(mon_hp,mon_dex,mon_weapon,STR #DEX #CON #INT #WIS #CHA, HP #inventory)- player and montser fight to death, the final boss cronos can attack 6 times as a hexaweilder, for every map that you have been to he looses one of his 6 arms, he also looses a turn
+    #combat(mon_hp,mon_dex,mon_weapon,STR #DEX, HP #inventory)- player and montser fight to death, the final boss cronos can attack 6 times as a hexaweilder, for every map that you have been to he looses one of his 6 arms, he also looses a turn
     #if you defeat him you win
     # if you loose sad dialouge
 """-------------------------------------------------------------------------------------------------------------------------------"""
@@ -44,18 +44,24 @@
     # bad avarage you gain a hat with curse of binding that removes -2 charisma
     # if medium you gain slippers removes the intimidation effect from you
     # if good you gain (ice ring to cast spells)
+    #1D10
 """-------------------------------------------------------------------------------------------------------------------------------"""
-# Items 
-# ice ring to cast spells
+# if the boss is dead use function Item shop
+    #dialouge
+    # what would you like to buy? 
+    # #display list of shoppabl items ((Bootle of XP updates player stats +2)- 1 Gold (Great sword (3d8) strength, higher chance of missing)- 1 Gold  (Rapier Dexterity weapon)(1d8)- 1 Gold  (Wand ability to cast “start shooter” blinds enemy and deals 1d6 damage)- 1 Gold  (Armor +5 AC)- 2 Gold  )
+"""-------------------------------------------------------------------------------------------------------------------------------"""
+# Items/ variables
+# ice ring to cast spells, -3 dex to enemy
 # slippers removes the intimidation effect from you
 # a hat with curse of binding that removes -2 charisma
 # gold
 # ability : Gorilla warfare
-# The Rapier of sacrifice ( Stab yourself stabs your enemy, stabbing your enemy stabs yourself)
+# The Rapier of sacrifice ( Stab yourself stabs your enemy, stabbing your enemy stabs yourself,) (2d20)
 # Graviole wand ( permanent +2 dexterity bonus) (force attack)
 # Meat
 # intimidated spell
-# Cyber chip ( grants +2 intelligence permanently)
+# Cyber chip ( grants +2 Dexterity permanently)
 # cronos ‘s ability is to turn back one turn in time
 # Bootle of XP (updates player stats +2)
 # Great sword (3d8)( strength, higher chance of missing)
@@ -63,5 +69,6 @@
 # Wand ability to cast “start shooter”( blinds enemy and deals 1d6 damage)
 # Armor +5 AC 
 # sword
+
 """-------------------------------------------------------------------------------------------------------------------------------"""
 
