a6e83c438424cbf07d56035e4495bd8bd722a589
INTELION11 <jacob.quezada@ucas-edu.net>
2025-12-05 09:31:12 -0700

    speudo nito

--- projects/Final_Pseudocode.py
+++ projects/Final_Pseudocode.py
@@ -5,37 +5,36 @@
 # create inventory as a list, all items that can be used in hand are stored here for atack and healing
 # (f"\033[38;2;160;160;160mWelcome adventurer, your name is lyte\033[1m") - make the text color light grey
 # randomize     #STR #DEX 1-20 and #HP 60-70
-# stat_opinion = display input f"here are your stats: {STR DEX HP}, tyou have one more chance to re roll, say yes if you want to reroll, say no if you dont"
+# stat_opinion = show input "here are your stats: STR DEX HP, tyou have one more chance to re roll, say yes if you want to reroll, say no if you dont"
 # if stat_opinion == yes
 # randomize     #STR #DEX 1-20 and #HP 60-70
-    # display f"here are your stats {STR DEX HP}""
+    # show "here are your stats STR DEX HP""
 # elif stat_opinion == no
-    #display ""
+    #show ""
 # else
-    #display""
-# display " Cut scene "
+    #show""
+# show " Cut scene " 
+# function of Desert_hammurabi
 """-------------------------------------------------------------------------------------------------------------------------------"""
 #    define desert_hamurabii,The iztec Jungla,mlɒɘЯbɘqɿɒWɘ⑁TTheWarpedrealm,Pangeon,Modern world,Cybercity 3012,Medieval Europe -(choose to fight or wait 2:30 minutes to be teleported anywhere else -modern world)  (STR #DEX  #INT #WIS #CHA, HP #inventory)
-    #   display cutscene (descriptive dialouge of the place)
+    #   show cutscene (descriptive dialouge of the place)
     #   mon_hp = 34(example)
     #    mon_dex = 13(example)
     #   mon_weapon = body (example)
-    #combat(Inventory,mon_hp,mon_dex,mon_weapon,STR #DEX, HP #inventory)- player and montser fight to death,(add Weapons and armor modifiers) (exaustion score for- desert), basic combat function created in a nother project with sligt diferenciations (multiple monsters and -4 inteligence- Cybercity 3012), (every 2 turns, oxygen poisoning 1d4 - Pangeon)(+4 dex-TheWarpedrealm)(Intimadated(Disadvantage) and -4 STR- Modern world).to use on diferent maps
+    #combat(Inventory,mon_hp,mon_dex,mon_weapon,STR #DEX, HP #inventory)- player and montser fight to death, (they can choose to use anything in their inventory)(add Weapons and armor modifiers) 
+    # (exaustion score for- desert), basic combat function created in a nother project with sligt diferenciations (multiple monsters and -4 inteligence- Cybercity 3012), 
+    # (every 2 turns, oxygen poisoning 1d4 - Pangeon)(+4 dex-TheWarpedrealm)(Intimadated(Disadvantage) and -4 STR- Modern world).to use on diferent maps
     #if monster = is dead
     #   add to inventory (meat,gold,time_cristal, ancient artifact(depends on the map))
-    #   display ( inventory updates: {inventory}
-    # open shop keeper, dialouge and display items to buy from him, exit function, made using the grocery list from another project
+    #   show ( inventory updates: {inventory}
+    # open shop keeper, dialouge and show items to buy from him, exit function, made using the grocery list from another project
     #if the player is dead loop universe to the start
-    #   crastal_option is "do you want to go though portal 1 or 2"
-    #if 1
-    #  option one is random between 1-8,  the number decides what world they teleport into apart from the world they have been to
-    # if 2 go to final boss cronos's world
 """-------------------------------------------------------------------------------------------------------------------------------"""
 # define Final Boss
     # cronos dialouge dialouge 
     #combat(mon_hp,mon_dex,mon_weapon,STR #DEX, HP #inventory)- player and montser fight to death, the final boss cronos can attack 6 times as a hexaweilder, for every map that you have been to he looses one of his 6 arms, he also looses a turn
-    #if you defeat him you win
-    # if you loose sad dialouge
+    #if you defeat him you win, give option to restart
+    # if you loose sad dialouge, give option to restart
 """-------------------------------------------------------------------------------------------------------------------------------"""
 # define Ötzi’s Tusdra
     # dialouge
@@ -49,14 +48,23 @@
 # if the boss is dead use function Item shop
     #dialouge
     # what would you like to buy? 
-    # #display list of shoppabl items ((Bootle of XP updates player stats +2)- 1 Gold (Great sword (3d8) strength, higher chance of missing)- 1 Gold  (Rapier Dexterity weapon)(1d8)- 1 Gold  (Wand ability to cast “start shooter” blinds enemy and deals 1d6 damage)- 1 Gold  (Armor +5 AC)- 2 Gold  )
+    # #show list of shoppable items ((Bootle of XP updates player stats +2)- 1 Gold (Great sword (3d8) strength, higher chance of missing)- 1 Gold  
+    # (Rapier Dexterity weapon)(1d8)- 1 Gold  (Wand ability to cast “start shooter” blinds enemy and deals 1d6 damage)- 1 Gold  (Armor +5 AC)- 2 Gold  )
 """-------------------------------------------------------------------------------------------------------------------------------"""
+#How it runs:
+#While there are still discoverable worlds
+    #   cristal_option is "do you want to go though portal 1 (random) or 2 (cronos's realm)"
+    #if you go through potal 1
+    #  option one is random between 1-8,  the number decides what world they teleport into (apart from the world they have been to, if there are none left then break)
+    # if you go thpough portal 2 go to final boss cronos's world
+"""-------------------------------------------------------------------------------------------------------------------------------"""
+# use function Final Boss if worlds 1-8 are discovered already
 # Items/ variables
 # ice ring to cast spells, -3 dex to enemy
 # slippers removes the intimidation effect from you
 # a hat with curse of binding that removes -2 charisma
 # gold
-# ability : Gorilla warfare
+# ability : Gorilla warfare: strike twice becouse enemy is surprised
 # The Rapier of sacrifice ( Stab yourself stabs your enemy, stabbing your enemy stabs yourself,) (2d20)
 # Graviole wand ( permanent +2 dexterity bonus) (force attack)
 # Meat
@@ -69,6 +77,7 @@
 # Wand ability to cast “start shooter”( blinds enemy and deals 1d6 damage)
 # Armor +5 AC 
 # sword
+# cronos teleport (teleport into cronos's realm)
 
 """-------------------------------------------------------------------------------------------------------------------------------"""
 
